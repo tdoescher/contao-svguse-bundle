e4dd94399ba906683dc9694047a94a37ff95d61f
Torben Döscher <mail@tdoescher.de>
2022-02-09 11:54:11 +0000

    Added Support for custon CSS classes
    {{svgicon::icon::custom,classes}}
    {{svguse::icon::custom,classes}}

--- src/EventListener/ReplaceInsertTagsListener.php
+++ src/EventListener/ReplaceInsertTagsListener.php
@@ -14,28 +14,30 @@ class ReplaceInsertTagsListener
     {
         $list = explode('::', $tag);
         $tag = $list[0];
-        $value = isset($list[1]) ? $list[1] : false;
+        $icon = isset($list[1]) ? $list[1] : false;
+        $class = isset($list[2]) ? $list[2] : false;
+        $classes = ($class) ? 'icon-'.$icon.' '.str_replace(',', ' ', trim($class)) : 'icon-'.$icon;
 
-        if(!in_array($tag, ['svgicon', 'svguse', 'svgimport']) && $value === false)
+        if(!in_array($tag, ['svgicon', 'svguse', 'svgimport']) && $icon === false)
         {
             return false;
         }
 
         if ($tag === 'svgicon')
         {
-            return '<span class="icon-'.$value.'"></span>';
+            return '<span class="'.$classes.'"></span>';
         }
 
         if ($tag === 'svguse')
         {
-            return '<svg class="icon icon-'.$value.'"><use xlink:href="'.Environment::get('request').'#icon-'.$value.'"></use></svg>';
+            return '<svg class="icon '.$classes.'"><use xlink:href="'.Environment::get('request').'#icon-'.$icon.'"></use></svg>';
         }
 
         if ($tag === 'svgimport')
         {
-            if(file_exists(Environment::get('documentRoot').'/../files/'.$value.'.svg'))
+            if(file_exists(Environment::get('documentRoot').'/../files/'.$icon.'.svg'))
             {
-                return file_get_contents(Environment::get('documentRoot').'/../files/'.$value.'.svg');
+                return file_get_contents(Environment::get('documentRoot').'/../files/'.$icon.'.svg');
             }
             else
             {
