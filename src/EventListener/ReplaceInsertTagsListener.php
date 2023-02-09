f803545cdd517b08f5c70d5c1a6853603abc112c
Torben Döscher <mail@tdoescher.de>
2023-02-09 19:20:40 +0000

    Update to PHP attributes, requires PHP8 and Contao 4.13

--- src/EventListener/ReplaceInsertTagsListener.php
+++ src/EventListener/ReplaceInsertTagsListener.php
@@ -11,36 +11,36 @@
 
 namespace tdoescher\SvgUseBundle\EventListener;
 
-use Contao\CoreBundle\ServiceAnnotation\Hook;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\Environment;
 
 #[AsHook('replaceInsertTags', priority: 100)]
 class ReplaceInsertTagsListener
 {
-  public function __invoke(string $tag)
+  public function __invoke(string $insertTag)
   {
-    $list = explode('::', $tag);
-    $tag = $list[0];
+    $list = explode('::', $insertTag);
+    $insertTag = $list[0];
     $icon = isset($list[1]) ? $list[1] : false;
     $class = isset($list[2]) ? $list[2] : false;
     $classes = ($class) ? 'icon-'.$icon.' '.str_replace(',', ' ', trim($class)) : 'icon-'.$icon;
 
-    if(!in_array($tag, ['svgicon', 'svguse', 'svgimport']) || $icon === false)
+    if(!in_array($insertTag, ['svgicon', 'svguse', 'svgimport']) || $icon === false)
     {
       return false;
     }
 
-    if ($tag === 'svgicon')
+    if ($insertTag === 'svgicon')
     {
       return '<span class="'.$classes.'"></span>';
     }
 
-    if ($tag === 'svguse')
+    if ($insertTag === 'svguse')
     {
       return '<svg class="icon '.$classes.'"><use xlink:href="'.Environment::get('request').'#icon-'.$icon.'"></use></svg>';
     }
 
-    if ($tag === 'svgimport' && file_exists(Environment::get('documentRoot').'/../files/'.$icon.'.svg'))
+    if ($insertTag === 'svgimport' && file_exists(Environment::get('documentRoot').'/../files/'.$icon.'.svg'))
     {
       return file_get_contents(Environment::get('documentRoot').'/../files/'.$icon.'.svg');
     }
